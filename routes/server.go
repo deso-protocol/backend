c16d175d64ad7c1ed009ff69eb7ec843cdf0847d
Lazy Nina <81658138+lazynina@users.noreply.github.com>
2021-06-10 16:17:22 -0700

    Get single profile picture endpoint (#40)
    * add get single profile picture endpoint, remove profile pic from profile entry response to lighten payloads
    
    * remove UsersWhoHODLYou from get users stateless
    
    * add users who hodl you count to support the sell-your-own-coin warning
    
    * update get single profile pic to return an image file instead of base64 string

--- routes/server.go
+++ routes/server.go
@@ -55,6 +55,7 @@ const (
 	RoutePathDeleteIdentities         = "/api/v0/delete-identities"
 	RoutePathGetProfiles              = "/api/v0/get-profiles"
 	RoutePathGetSingleProfile         = "/api/v0/get-single-profile"
+	RoutePathGetSingleProfilePicture  = "/api/v0/get-single-profile-picture"
 	RoutePathGetHodlersForPublicKey   = "/api/v0/get-hodlers-for-public-key"
 	RoutePathGetDiamondsForPublicKey  = "/api/v0/get-diamonds-for-public-key"
 	RoutePathGetFollowsStateless      = "/api/v0/get-follows-stateless"
@@ -457,6 +458,13 @@ func (fes *APIServer) NewRouter() *muxtrace.Router {
 			[]string{"POST", "OPTIONS"},
 			RoutePathGetSingleProfile,
 			fes.GetSingleProfile,
+			PublicAccess,
+		},
+		{
+			"GetSingleProfilePicture",
+			[]string{"GET"},
+			RoutePathGetSingleProfilePicture + "/{publicKeyBase58Check:[0-9a-zA-Z]{54,55}}",
+			fes.GetSingleProfilePicture,
 			PublicAccess,
 		},
 		{
