2db70b9e4edda3abcaf9c48c5109b14d34a85b30
lazynina <lazynina84@gmail.com>
2021-08-16 23:54:36 -0700

    clean up variable names and move functions to admin_tutorial.go and tutorial.go

--- routes/admin_tutorial.go
+++ routes/admin_tutorial.go
@@ -41,7 +41,7 @@ func (fes *APIServer) AdminUpdateTutorialCreator(ww http.ResponseWriter, req *ht
 		} else {
 			tableCreatorIn = "up and coming"
 		}
-		_AddBadRequestError(ww, fmt.Sprintf("AdminUpdateTutorialCreator: cannot add creator to %v, user already exists in other index", tableCreatorIn))
+		_AddBadRequestError(ww, fmt.Sprintf("AdminUpdateTutorialCreator: cannot add creator, user already exists in %v index", tableCreatorIn))
 		return
 	}
 
@@ -50,21 +50,21 @@ func (fes *APIServer) AdminUpdateTutorialCreator(ww http.ResponseWriter, req *ht
 		_AddBadRequestError(ww, fmt.Sprintf("AdminUpdateTutorialCreator: No PKID found for public key: %v", requestData.PublicKeyBase58Check))
 		return
 	}
-	var prefix []byte
+	var key []byte
 	if requestData.IsWellKnown {
-		prefix = GlobalStateKeyWellKnownTutorialCreators(pkid.PKID)
+		key = GlobalStateKeyWellKnownTutorialCreators(pkid.PKID)
 	} else {
-		prefix = GlobalStateKeyUpAndComingTutorialCreators(pkid.PKID)
+		key = GlobalStateKeyUpAndComingTutorialCreators(pkid.PKID)
 	}
 
 	if requestData.IsRemoval {
-		if err := fes.GlobalStateDelete(prefix); err != nil {
+		if err = fes.GlobalStateDelete(key); err != nil {
 			_AddBadRequestError(ww, fmt.Sprintf(
 				"AdminUpdateTutorialCreator: Error processing GlobalStateDelete: %v", err))
 			return
 		}
 	} else {
-		if err := fes.GlobalStatePut(prefix, []byte{1}); err != nil {
+		if err = fes.GlobalStatePut(key, []byte{1}); err != nil {
 			_AddBadRequestError(ww, fmt.Sprintf(
 				"AdminUpdateTutorialCreator: Error processing GlobalStatePut: %v", err))
 			return
@@ -72,16 +72,39 @@ func (fes *APIServer) AdminUpdateTutorialCreator(ww http.ResponseWriter, req *ht
 	}
 
 	// Reset all userMetadata values related to tutorial featured status.
-	if requestData.IsWellKnown {
-		userMetadata.IsFeaturedTutorialWellKnownCreator = !requestData.IsRemoval
-		userMetadata.IsFeaturedTutorialUpAndComingCreator = false
-	} else {
-		userMetadata.IsFeaturedTutorialUpAndComingCreator = !requestData.IsRemoval
-		userMetadata.IsFeaturedTutorialWellKnownCreator = false
-	}
+	userMetadata.IsFeaturedTutorialWellKnownCreator = requestData.IsWellKnown && !requestData.IsRemoval
+	userMetadata.IsFeaturedTutorialUpAndComingCreator = !requestData.IsWellKnown && !requestData.IsRemoval
 
 	if err = fes.putUserMetadataInGlobalState(userMetadata); err != nil {
 		_AddBadRequestError(ww, fmt.Sprintf("AdminUpdateTutorialCreator: Problem putting updated user metadata in Global state: %v", err))
 		return
+	}
+}
+
+type AdminResetTutorialStatusRequest struct {
+	PublicKeyBase58Check string
+}
+
+func (fes *APIServer) AdminResetTutorialStatus(ww http.ResponseWriter, req *http.Request) {
+	decoder := json.NewDecoder(io.LimitReader(req.Body, MaxRequestBodySizeBytes))
+	requestData := AdminResetTutorialStatusRequest{}
+	if err := decoder.Decode(&requestData); err != nil {
+		_AddBadRequestError(ww, fmt.Sprintf("AdminResetTutorialStatus: Problem parsing request body: %v", err))
+		return
+	}
+
+	userMetadata, err := fes.getUserMetadataFromGlobalState(requestData.PublicKeyBase58Check)
+	if err != nil {
+		_AddBadRequestError(ww, fmt.Sprintf("AdminResetTutorialStatus: Error getting user metadata from global state: %v", err))
+		return
+	}
+
+	if userMetadata.TutorialStatus != EMPTY && userMetadata.CreatorPurchasedInTutorialPKID != nil {
+		userMetadata.TutorialStatus = EMPTY
+		userMetadata.CreatorPurchasedInTutorialPKID = nil
+		if err = fes.putUserMetadataInGlobalState(userMetadata); err != nil {
+			_AddBadRequestError(ww, fmt.Sprintf("AdminResetTutorialStatus: Error putting user metadata in global state: %v", err))
+			return
+		}
 	}
 }
--- routes/tutorial.go
+++ routes/tutorial.go
@@ -11,7 +11,6 @@ import (
 
 type GetTutorialCreatorsRequest struct {
 	ResponseLimit int
-	JWT       string
 }
 
 
@@ -36,18 +35,22 @@ func (fes *APIServer) GetTutorialCreators(ww http.ResponseWriter, req *http.Requ
 		_AddBadRequestError(ww, fmt.Sprintf("GetTutorialCreators: Error getting utxoView: %v", err))
 		return
 	}
-	upAndComingProfileEntryResponses, err := fes.GetFeaturedCreators(utxoView, requestData.ResponseLimit, upAndComingSeekKey)
+	// Grab verified username map pointer
+	verifiedMap, err := fes.GetVerifiedUsernameToPKIDMap()
+	if err != nil {
+		_AddBadRequestError(ww, fmt.Sprintf("GetTutorialCreators: Problem fetching verifiedMap: %v", err))
+		return
+	}
+	upAndComingProfileEntryResponses, err := fes.GetFeaturedCreators(utxoView, requestData.ResponseLimit, upAndComingSeekKey, verifiedMap)
 	if err != nil {
 		_AddBadRequestError(ww, fmt.Sprintf("GetTutorialCreators: Problem getting up and coming tutorial creators: %v", err))
 		return
 	}
-	wellKnownProfileEntryResponses, err := fes.GetFeaturedCreators(utxoView, requestData.ResponseLimit, wellKnownSeekKey)
+	wellKnownProfileEntryResponses, err := fes.GetFeaturedCreators(utxoView, requestData.ResponseLimit, wellKnownSeekKey, verifiedMap)
 	if err != nil {
 		_AddBadRequestError(ww, fmt.Sprintf("GetTutorialCreators: Problem getting well known tutorial creators: %v", err))
 		return
 	}
-
-
 
 	res := GetTutorialCreatorResponse{
 		UpAndComingProfileEntryResponses: upAndComingProfileEntryResponses,
@@ -59,7 +62,7 @@ func (fes *APIServer) GetTutorialCreators(ww http.ResponseWriter, req *http.Requ
 	}
 }
 
-func (fes *APIServer) GetFeaturedCreators(utxoView *lib.UtxoView, responseLimit int, seekKey []byte) (_profileEntryResponses []ProfileEntryResponse, _err error) {
+func (fes *APIServer) GetFeaturedCreators(utxoView *lib.UtxoView, responseLimit int, seekKey []byte, verifiedMap map[string]*lib.PKID) (_profileEntryResponses []ProfileEntryResponse, _err error) {
 	maxKeyLen := 1 + btcec.PubKeyBytesLenCompressed
 	keys, _, err := fes.GlobalStateSeek(
 		seekKey,
@@ -86,15 +89,60 @@ func (fes *APIServer) GetFeaturedCreators(utxoView *lib.UtxoView, responseLimit 
 	for _, dbKeyBytes := range keys[:publicKeysUpperBound] {
 		// Chop the public key out of the db key.
 		// The dbKeyBytes are: [One Prefix Byte][btcec.PubKeyBytesLenCompressed]
-		publicKeyBytes := dbKeyBytes[1 :][:]
+		publicKeyBytes := dbKeyBytes[1:][:]
 		profileEntryy := utxoView.GetProfileEntryForPublicKey(publicKeyBytes)
-		// Grab verified username map pointer
-		verifiedMap, err := fes.GetVerifiedUsernameToPKIDMap()
-		if err != nil {
-			return nil, fmt.Errorf("GetFeaturedCreators: Problem fetching verifiedMap: %v", err)
-		}
 		profileEntryResponse := _profileEntryToResponse(profileEntryy, fes.Params, verifiedMap, utxoView)
 		profileEntryResponses = append(profileEntryResponses, *profileEntryResponse)
 	}
 	return profileEntryResponses, nil
+}
+
+type StartOrSkipTutorialRequest struct {
+	PublicKeyBase58Check string
+	JWT string
+	IsSkip bool
+}
+
+func (fes *APIServer) StartOrSkipTutorial(ww http.ResponseWriter, req *http.Request) {
+	decoder := json.NewDecoder(io.LimitReader(req.Body, MaxRequestBodySizeBytes))
+	requestData := StartOrSkipTutorialRequest{}
+	if err := decoder.Decode(&requestData); err != nil {
+		_AddBadRequestError(ww, fmt.Sprintf(
+			"StartOrSkipTutorial: Problem parsing request body: %v", err))
+		return
+	}
+	isValid, err := fes.ValidateJWT(requestData.PublicKeyBase58Check, requestData.JWT)
+	if err != nil {
+		_AddBadRequestError(ww, fmt.Sprintf("StartOrSkipTutorialioBegin: Error validating JWT: %v", err))
+		return
+	}
+	if !isValid {
+		_AddBadRequestError(ww, fmt.Sprintf("StartOrSkipTutorial: Invalid token: %v", err))
+		return
+	}
+
+	userMetadata, err := fes.getUserMetadataFromGlobalState(requestData.PublicKeyBase58Check)
+	if err != nil {
+		_AddBadRequestError(ww, fmt.Sprintf("StartOrSkipTutorial: Error getting user metadata from global state: %v", err))
+		return
+	}
+
+	if requestData.IsSkip && userMetadata.TutorialStatus != EMPTY {
+		_AddBadRequestError(ww, fmt.Sprintf("StartOrSkipTutorial: Can only skip tutorial from empty state"))
+		return
+	}
+	if !requestData.IsSkip && userMetadata.TutorialStatus != EMPTY && userMetadata.TutorialStatus != SKIPPED {
+		_AddBadRequestError(ww, fmt.Sprintf("StartOrSkipTutorial: Can only start tutorial from empty or skipped state"))
+		return
+	}
+
+	if requestData.IsSkip {
+		userMetadata.TutorialStatus = SKIPPED
+	} else {
+		userMetadata.TutorialStatus = STARTED
+	}
+	if err = fes.putUserMetadataInGlobalState(userMetadata); err != nil {
+		_AddBadRequestError(ww, fmt.Sprintf("StartOrSkipTutorial: err putting user metdata in global state: %v", err))
+		return
+	}
 }
