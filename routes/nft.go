9c3d2d1bce764c6a6c38103e3dfaf16c3ff1dd32
kanshi <andrzej.tucholka@gmail.com>
2021-10-23 20:45:35 +0200

    Fix GetNFTShowcase endpoint iterating over burned NFTs (#183)

--- routes/nft.go
+++ routes/nft.go
@@ -729,6 +729,10 @@ func (fes *APIServer) GetNFTShowcase(ww http.ResponseWriter, req *http.Request) 
 			return
 		}
 
+		if postEntry.NumNFTCopiesBurned == postEntry.NumNFTCopies {
+			continue
+		}
+
 		nftKey := lib.MakeNFTKey(nftHash, 1)
 		nftEntry := utxoView.GetNFTEntryForNFTKey(&nftKey)
 
