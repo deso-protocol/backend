b955d082102a0329946f10e0111d8ecc9250481e
Piotr Nojszewski <29924594+AeonSw4n@users.noreply.github.com>
2022-01-19 16:48:17 -0500

    Add backend2

--- scripts/tools/compare_databaes.go
+++ scripts/tools/compare_databaes.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"github.com/deso-protocol/backend/scripts/tools/toolslib"
 	"github.com/deso-protocol/core/lib"
+		"time"
 )
 
 func main() {
-	dir0 := "/Users/piotr/data_dirs/n0_1"
-	dir1 := "/Users/piotr/data_dirs/n1_1"
+	dir0 := "/Users/piotr/data_dirs/n4_1"
+	dir1 := "/Users/piotr/data_dirs/n5_1"
+	//dir2 := "/Users/piotr/data_dirs/n7_1"
 
 	db0, err := toolslib.OpenDataDir(dir0)
 	if err != nil {
@@ -21,44 +23,74 @@ func main() {
 		fmt.Printf("Error reading db1 err: %v", err)
 		return
 	}
+	//db2, err := toolslib.OpenDataDir(dir2)
+	//if err != nil {
+	//	fmt.Printf("Error reading db2 err: %v", err)
+	//	return
+	//}
 
 	//snap, _ := lib.NewSnapshot(100000)
 	//fmt.Println(snap.GetMostRecentSnapshot(db0, []byte{5}, []byte{5}))
 	//fmt.Println(snap.GetMostRecentSnapshot(db1, []byte{5}, []byte{5}))
 	maxBytes := uint32(8<<20)
 	totalLen := 0
+	var timeElapsed float64
+	var currentTime time.Time
+	timeElapsed = 0.0
+	currentTime = time.Now()
 	err = func() error {
 		fmt.Printf("Checking prefixes: ")
 		for _, prefix := range lib.StatePrefixes {
 			fmt.Printf("%v ", prefix)
 			lastPrefix := prefix
 			for {
+				timeElapsed += time.Since(currentTime).Seconds()
+				currentTime = time.Now()
+				//fmt.Println("Starting the fetch time elapsed (%v) current time (%v)", timeElapsed, currentTime)
 				k0, v0, full0, err := lib.DBIteratePrefixKeys(db0, prefix, lastPrefix, maxBytes)
-				if err != nil {
-					return fmt.Errorf("Error reading db0 err: %v\n", err)
-				}
+				//timeElapsed += time.Since(currentTime).Seconds()
+				//currentTime = time.Now()
+				//fmt.Println("Starting the fetch time elapsed (%v) current time (%v)", timeElapsed, currentTime)
+				//if err != nil {
+				//	return fmt.Errorf("Error reading db0 err: %v\n", err)
+				//}
+				//fmt.Println("Current key %v", (*k0)[0])
+				//db2.Update(func(txn *badger.Txn) error {
+                //	for i, _ := range *k0 {
+				//		keyBytes, _ := hex.DecodeString((*k0)[i])
+				//		valueBytes, _ := hex.DecodeString((*v0)[i])
+				//		lib.DBSetWithTxn(txn, nil, keyBytes, valueBytes)
+				//	}
+				//	return nil
+				//})
+				//timeElapsed += time.Since(currentTime).Seconds()
+				//currentTime = time.Now()
+				//fmt.Println("Finished writing data time elapsed (%v) current time (%v)", timeElapsed, currentTime)
+
+
 				k1, v1, full1, err := lib.DBIteratePrefixKeys(db1, prefix, lastPrefix, maxBytes)
 				if err != nil {
 					return fmt.Errorf("Error reading db1 err: %v\n", err)
 				}
 				if len(*k0) != len(*k1) {
-					return fmt.Errorf("Databases not equal on prefix: %v, and lastPrefix: %v;" +
+					fmt.Printf("Databases not equal on prefix: %v, and lastPrefix: %v;" +
 						"varying lengths (db0, db1) : (%v, %v)\n", prefix, lastPrefix, len(*k0), len(*k1))
+					break
 				}
-				for ii, key := range *k1 {
+				for ii, key := range *k0 {
 					if key != (*k1)[ii] {
-						return fmt.Errorf("Databases not equal on prefix: %v, and lastPrefix: %v;" +
+						fmt.Printf("Databases not equal on prefix: %v, and lastPrefix: %v;" +
 							"unequal keys (db0, db1) : (%v, %v)\n", prefix, lastPrefix, key, (*k1)[ii])
 					}
 				}
-				for ii, value := range *v1 {
+				for ii, value := range *v0 {
 					if value != (*v1)[ii] {
-						return fmt.Errorf("Databases not equal on prefix: %v, and lastPrefix: %v;" +
+						fmt.Printf("Databases not equal on prefix: %v, and lastPrefix: %v;" +
 							"unequal values (db0, db1) : (%v, %v)\n", prefix, lastPrefix, value, (*v1)[ii])
 					}
 				}
 				if full0 != full1 {
-					return fmt.Errorf("Databases not equal on prefix: %v, and lastPrefix: %v;" +
+					fmt.Printf("Databases not equal on prefix: %v, and lastPrefix: %v;" +
 						"unequal fulls (db0, db1) : (%v, %v)\n", prefix, lastPrefix, full0, full1)
 				}
 				//fmt.Println("lastPrefix", lastPrefix, "full", full0, len(*k0))
