db74521a100d7a4f48808225469f3ec589f610bc
diamondhands <diamondhands@bitcloutdev.com>
2022-02-26 01:44:19 -0800

    diamondhands touchups

--- routes/transaction.go
+++ routes/transaction.go
@@ -2686,7 +2686,7 @@ func (fes *APIServer) AuthorizeDerivedKey(ww http.ResponseWriter, req *http.Requ
 	var memo []byte
 	blockHeight := fes.blockchain.BlockTip().Height + 1
 	// Only add the TransactionSpendingLimit and Memo if we're passed the block height.
-	if fes.Params.ForkHeights.DerivedKeySetSpendingLimitsBlockHeight < blockHeight {
+	if blockHeight >= fes.Params.ForkHeights.DerivedKeySetSpendingLimitsBlockHeight {
 		transactionSpendingLimit, err = fes.ToTransactionSpendingLimit(requestData.TransactionSpendingLimit)
 		if err != nil {
 			_AddBadRequestError(ww, fmt.Sprintf("AuthorizeDerivedKey: Error parsing TransactionSpendingLimit: %v", err))
