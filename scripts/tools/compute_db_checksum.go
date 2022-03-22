40fa37571ed3f46cb598eac7871c69eb503a4b46
Piotr Nojszewski <29924594+AeonSw4n@users.noreply.github.com>
2022-03-22 19:33:06 -0400

    Compute checksum script

---
+++ scripts/tools/compute_db_checksum.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"github.com/deso-protocol/backend/scripts/tools/toolslib"
+	"github.com/deso-protocol/core/lib"
+	"sort"
+)
+
+func main() {
+	dbDir := "/Users/piotr/data_dirs/hypersync/sentry"
+
+	db, err := toolslib.OpenDataDir(dbDir)
+	if err != nil {
+		fmt.Printf("Error reading db1 err: %v", err)
+		return
+	}
+	snap, err := lib.NewSnapshot(dbDir, lib.SnapshotBlockHeightPeriod, false, false)
+	if err != nil {
+		fmt.Printf("Error reading snap err: %v", err)
+		return
+	}
+	snap.CurrentEpochSnapshotMetadata.SnapshotBlockHeight = 114000
+	snap.Checksum.Initialize()
+
+	maxBytes := uint32(8 << 20)
+	var prefixes [][]byte
+	for prefix, _ := range lib.StatePrefixes.StatePrefixesMap {
+		if prefix == 14 || prefix == 15 || prefix == 16 || prefix == 42 {
+			continue
+		}
+
+		prefixes = append(prefixes, []byte{prefix})
+	}
+	sort.Slice(prefixes, func(ii, jj int) bool {
+		return prefixes[ii][0] < prefixes[jj][0]
+	})
+	fmt.Println(prefixes)
+	err = func() error {
+		fmt.Printf("Checking prefixes: ")
+		for _, prefix := range prefixes {
+			fmt.Printf("%v \n", prefix)
+			lastPrefix := prefix
+			var recurr func()
+			recurr = func() {
+				entries, fullDb, err := lib.DBIteratePrefixKeys(db, prefix, lastPrefix, maxBytes)
+				if err != nil {
+					panic(fmt.Errorf("Problem fetching snapshot chunk (%v)", err))
+				}
+				for _, entry := range entries {
+					snap.AddChecksumBytes(lib.EncodeKeyValue(entry.Key, entry.Value))
+				}
+
+				if len(entries) != 0 {
+					lastPrefix = entries[len(entries)-1].Key
+				} else if fullDb {
+					panic("Number of ancestral records should not be zero")
+				}
+
+				if fullDb {
+					recurr()
+				}
+			}
+			recurr()
+			snap.WaitForAllOperationsToFinish()
+			checksumBytes, _ := snap.Checksum.ToBytes()
+			fmt.Println("prefix", prefix, "checksum:", checksumBytes)
+
+		}
+		fmt.Println("Finished iterating all prefixes")
+		snap.WaitForAllOperationsToFinish()
+		checksumBytes, _ := snap.Checksum.ToBytes()
+		fmt.Println("Final checksum:", checksumBytes)
+		return nil
+	}()
+
+}
