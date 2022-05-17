13817c3e6c391761d6cf98dcaceb83e426e4f740
lazynina <lazynina84@gmail.com>
2022-05-17 11:55:07 -0400

    Skip nil BidEntries in GetAcceptedBidHistory

--- routes/nft.go
+++ routes/nft.go
@@ -2162,6 +2162,9 @@ func (fes *APIServer) GetAcceptedBidHistory(ww http.ResponseWriter, req *http.Re
 		}
 		var acceptedBidEntryResponses []*NFTBidEntryResponse
 		for _, acceptedBidEntry := range *acceptedBidEntries {
+			if acceptedBidEntry == nil {
+				continue
+			}
 			acceptedBidEntryResponses = append(acceptedBidEntryResponses,
 				fes._bidEntryToResponse(
 					acceptedBidEntry, nil, utxoView, false, false))
