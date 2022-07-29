f28dac6581684d56a2ff14101f48097f3f8bb0e0
Kevin Ayling <kevin@yourgem.app>
2022-07-29 10:39:43 -0700

    remove follow feed config

--- routes/exchange.go
+++ routes/exchange.go
@@ -1617,11 +1617,7 @@ func (fes *APIServer) GetPostsForFollowFeedForPublicKey(bav *lib.UtxoView, start
 		return nil, errors.Wrapf(err, "GetPostsForFollowFeedForPublicKey: Problem filtering out restricted public keys: ")
 	}
 
-	minTimestampNanos := uint64(time.Now().UTC().AddDate(0, 0, -fes.Config.FollowFeedLength).UnixNano()) // default: two days ago
-
-	if fes.Config.FollowFeedInfinite {
-		minTimestampNanos = 0 // Fetch all posts for infinite follow feed
-	}
+	minTimestampNanos := uint64(time.Now().UTC().AddDate(0, 0, -2).UnixNano()) // two days ago
 	// For each of these pub keys, get their posts, and load them into the view too
 	for _, followedPubKey := range filteredPubKeysMap {
 
