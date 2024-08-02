499b51db96cc09179380d7fa6bf4ddb39d99f66a
Lazy Nina <81658138+lazynina@users.noreply.github.com>
2024-08-02 10:17:56 -0400

    Get all global params (#662)

--- routes/server.go
+++ routes/server.go
@@ -210,6 +210,7 @@ const (
 
 	// admin_transaction.go
 	RoutePathGetGlobalParams                   = "/api/v0/get-global-params"
+	RoutePathGetAllGlobalParams                = "/api/v0/get-all-global-params"
 	RoutePathTestSignTransactionWithDerivedKey = "/api/v0/admin/test-sign-transaction-with-derived-key"
 
 	// Eventually we will deprecate the admin endpoint since it does not need to be protected.
@@ -667,6 +668,13 @@ func (fes *APIServer) NewRouter() *muxtrace.Router {
 			[]string{"POST", "OPTIONS"},
 			RoutePathGetGlobalParams,
 			fes.GetGlobalParams,
+			PublicAccess,
+		},
+		{
+			"GetAllGlobalParams",
+			[]string{"GET"},
+			RoutePathGetAllGlobalParams,
+			fes.GetAllGlobalParams,
 			PublicAccess,
 		},
 		// Route for sending DeSo
